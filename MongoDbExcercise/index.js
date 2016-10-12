d705cae8ec234ce03986f73674c2dbcd7dc77413
Yana Slavcheva <yana.slavcheva@gmail.com>
2016-10-12 15:01:28 +0300

    Added image and tag models

--- MongoDbExcercise/index.js
+++ MongoDbExcercise/index.js
@@ -1,7 +1,19 @@
 let mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
-// TODO: define models here
+
+let Image = mongoose.model('Image', {
+  url: {type: String},
+  creationDate: {type: Date, default: Date.now},
+  description: {type: String},
+  tags: [Tag.schema]
+})
+
+let Tag = mongoose.model('Tag', {
+  name: {type: String},
+  creationDate: {type: Date, default: Date.now},
+  images: [Image.schema]
+})
 
 let connection = 'mongodb://localhost:27017/imagestagsdb'
 
@@ -9,4 +21,6 @@ mongoose
 .connect(connection)
 .then(
   console.log('Connected to Mongoose')
+
+
 )
