333f0df5d5923def6410e49c8ae14c20467a66fa
Yana Slavcheva <yana.slavcheva@gmail.com>
2016-10-12 15:24:07 +0300

    Created example tag with image

--- MongoDbExcercise/index.js
+++ MongoDbExcercise/index.js
@@ -5,8 +5,7 @@ mongoose.Promise = global.Promise
 let Image = mongoose.model('Image', {
   url: {type: String},
   creationDate: {type: Date, default: Date.now},
-  description: {type: String},
-  tags: [Tag.schema]
+  description: {type: String}
 })
 
 let Tag = mongoose.model('Tag', {
@@ -19,8 +18,23 @@ let connection = 'mongodb://localhost:27017/imagestagsdb'
 
 mongoose
 .connect(connection)
-.then(
+.then(() => {
   console.log('Connected to Mongoose')
 
+  new Tag({
+    name: 'happy',
+    images: [
+      new Image({
+        url: 'http://cdn.pet360.com/pet360/Content/Images/CMS/Articles/Happy_Cat_Smiling.jpg',
+        description: 'small fluffy kitten'
+      })
+    ]
+  })
+  .save()
+  .catch(console.log)
 
-)
+  Tag
+    .find({})
+    .exec()
+    .then(tags => console.log(tags))
+})
