6cf61ee3a49eb40183669176fe7647404b0637bf
Yana Slavcheva <yana.slavcheva@gmail.com>
2016-12-16 16:11:08 +0000

    Added tweet model

---
+++ Exam-Twitter/express-architecture-master/server/data/Tweet.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+let requiredValidationMessage = '{PATH} is required'
+
+let tweetSchema = mongoose.Schema({
+    message: {type: String, requiredValidationMessage},
+    createdOn: {type: Date, default: Date.now}
+})
+
+tweetSchema.path('message').validate(function(value){
+    return value.length > 140
+}), 'Tweet too long'
+
+let Tweet = mongoose.model('Tweet', tweetSchema)
+
+module.exports.seedTweets = () => {
+    Tweet.find({}).then(tweets => {
+        if (tweets.length === 0) {
+            Tweet.create({
+                message: 'Tweet me baby one more time'
+            })
+        }
+    })
+}
