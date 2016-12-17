687a319c18f7357b5b42f3886383a0cd1c2a17fb
Yana Slavcheva <yana.slavcheva@gmail.com>
2016-12-17 10:39:09 +0200

    Adapted Tweet schema into Image schema

--- Exam-Instagram/server/config/config.js
+++ Exam-Instagram/server/config/config.js
@@ -5,7 +5,7 @@ let rootPath = path.normalize(path.join(__dirname, '/../../'))
 module.exports = {
   development: {
     rootPath: rootPath,
-    db: 'mongodb://localhost:27017/some-express-db',
+    db: 'mongodb://localhost:27017/insta-db',
     port: 1337
   },
   production: {
---
+++ Exam-Instagram/server/data/Image.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+let requiredValidationMessage = '{PATH} is required'
+let tooLongMessage = 'too long'
+
+let imageSchema = mongoose.Schema({
+    url: {type: String, requiredValidationMessage},
+    description: {type: String, requiredValidationMessage},
+    username: {type: String},
+    createdOn: {type: Date, default: Date.now}
+})
+
+imageSchema.path('url').validate(function(value){
+    return value.length < 500
+}), 'Image url ' + tooLongMessage
+
+imageSchema.path('description').validate(function(value){
+    return value.length < 500
+}), 'Image description ' + tooLongMessage
+
+let Image = mongoose.model('Image', imageSchema)
+
+module.exports.seedImages = () => {
+    Image.find({}).then(images => {
+        if (images.length === 0) {
+            Image.create(
+            {
+                username: 'Admin',
+                url: 'https://s-media-cache-ak0.pinimg.com/736x/5b/11/c6/5b11c6b5eabb728c76b652c330ddaf8e.jpg',
+                description: 'Super cute black kitten'
+            },
+            {
+                username: 'Admin',
+                url: 'http://cf.ltkcdn.net/cats/images/std/200777-425x322-kitten_crop.jpg',
+                description: 'Super cute orange kitten'
+            },
+            {
+                username: 'Admin',
+                url: 'http://kittentoob.com/wp-content/uploads/sites/4/2012/01/blue-eyed-white-persian-kitten.jpg',
+                description: 'Super cute white kitten'
+            })
+        }
+    })
+}
